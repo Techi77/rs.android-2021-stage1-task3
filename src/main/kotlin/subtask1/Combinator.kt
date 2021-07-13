cc7bce3a0977d378496890494286c8faf5ecab0d
Katharine Rossinskaya <rossinskaya98@gmail.com>
2021-07-13 16:41:54 +0300

    feat: add Combinator.kt

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,27 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        //для данного алгоритма есть формула (Сочетания без повторений) Akn=n!/(k!*(n-k)!),
+        //где Ank - количество плакатов, n - количество цветов, k - по сколько цветов на плакат
+        var Akn = array[0]
+        val n = array[1]
+        if (Akn >= 0 && n > 0) {
+            for(i in 0..n){
+                Akn = (factorial(n)/(factorial(i)* factorial(n-i))).toInt()
+                if (Akn == Akn){
+                    return (i)
+                }
+            }
+        }
+        return (null)
     }
+}
+fun factorial (num: Int): Long{
+    var result:Long = 1
+    for(i in 1..num){
+        result *= i
+    }
+    return result
 }
